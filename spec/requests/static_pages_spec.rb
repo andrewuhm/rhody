2d9cd406fac302645ff15636f93a02432911a4eb
Andrew Uhm <andrewuhm@gmail.com>
2013-07-30 16:38:19 -0700

    finish chp 5 exercises

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,31 +4,53 @@ describe "StaticPages" do
 
 	subject { page }
 
+	shared_examples_for "all static pages" do
+		it { should have_selector('h1', text: heading) }
+		it { should have_title(full_title(page_title)) }
+	end
+
 	describe "Home page" do
 		before { visit root_path }
+		let(:heading)	{ 'Quizzes' }
+		let(:page_title)	{ 'Home' }
 
-		it { should have_content('Quizzes') }
-		it { should have_title(full_title('Home')) }
+		it_should_behave_like "all static pages"
 	end
 
 	describe "Help page" do
 		before { visit help_path }
+		let(:heading) 	{ 'Help' }
+		let(:page_title) { 'Help' }
 
-		it { should have_content('Help') }
-		it { should have_title(full_title('Help')) }
+		it_should_behave_like "all static pages"
 	end
 
 	describe "About page" do
 		before { visit about_path }
+		let(:heading) { 'About Us' }
+		let(:page_title) { 'About Us' }
 
-		it { should have_content('About Us') }
-		it { should have_title(full_title('About Us')) }
+		it_should_behave_like "all static pages"
 	end
 
 	describe "Contact Us page" do
 		before { visit contact_path }
+		let(:heading) { 'Contact' }
+		let(:page_title) { 'Contact Us' }
 
-		it { should have_content('Contact') }
-		it { should have_title(full_title('Contact Us')) }
+		it_should_behave_like "all static pages"
+	end
+
+	it "should have the right links on the layout" do
+		visit root_path
+		click_link "About"
+		expect(page).to have_title(full_title('About Us'))
+		click_link "Help"
+		expect(page).to have_title(full_title('Help'))
+		click_link "Contact"
+		expect(page).to have_title(full_title('Contact Us'))
+		click_link "Home"
+		click_link "Sign up now!"
+		expect(page).to have_title(full_title('Sign Up'))
 	end
 end
