f171ad1cd6d09d56470fce6583deabf49290f560
Andrew Uhm <andrewuhm@gmail.com>
2013-07-30 14:00:12 -0700

    Add full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+	let(:base_title) { "Study Guide Quizzes" }
+
 	describe "Home page" do
 		it "should have the content 'Quizzes'" do
 			visit '/static_pages/home'
@@ -10,7 +12,7 @@ describe "StaticPages" do
 
 		it "should have the title 'Home'" do
 			visit '/static_pages/home'
-			expect(page).to have_title("Study Guide Quizzes | Home")
+			expect(page).to have_title("#{base_title} | Home")
 		end
 	end
 
@@ -22,7 +24,7 @@ describe "StaticPages" do
 
 		it "should have the title 'Help'" do
 			visit '/static_pages/help'
-			expect(page).to have_title("Study Guide Quizzes | Help")
+			expect(page).to have_title("#{base_title} | Help")
 		end
 	end
 
@@ -34,7 +36,19 @@ describe "StaticPages" do
 
 		it "should have the title 'About Us'" do
 			visit '/static_pages/about'
-			expect(page).to have_title("Study Guide Quizzes | About Us")
+			expect(page).to have_title("#{base_title} | About Us")
+		end
+	end
+
+	describe "Contact Us page" do
+		it "should have the content 'Contact us'" do
+			visit '/static_pages/contact'
+			expect(page).to have_content('Contact')
+		end
+
+		it "should have the title 'Contact Us'" do
+			visit '/static_pages/contact'
+			expect(page).to have_title("#{base_title} | Contact Us")
 		end
 	end
 end
